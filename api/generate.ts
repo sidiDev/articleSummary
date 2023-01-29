d61523d2ec0eb99bef19133deeac597c7c50f513
SidiDev <sididev3@gmail.com>
2023-01-29 23:55:09 +0000

    Fix some errors

--- api/generate.ts
+++ api/generate.ts
@@ -1,15 +1,32 @@
-import generateSummary from '@/lib/generateSummary';
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-export default function handler(
+import { Configuration, OpenAIApi } from "openai";
+
+const apiKey = process.env.VITE_OPENAI_API_KEY
+
+const configuration = new Configuration({
+  apiKey
+});
+
+export default async function handler(
   req: VercelRequest,
   res: VercelResponse,
 ) {
 
 const { promptChat } = req.body.data
-  generateSummary(promptChat).then(
-    (summary) => {
-      res.json({summary})
-    }
-  );
+
+
+    const openai = new OpenAIApi(configuration);
+  
+  const chatRes = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: promptChat,
+    temperature: 0.7,
+    max_tokens: 3065,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0
+  });
+
+  res.json({summary: chatRes.data.choices[0].text})
 }
