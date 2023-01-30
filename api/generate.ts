49306fe03e2be003ea1550487cb01a24ff4aa9ce
SidiDev <sididev3@gmail.com>
2023-01-30 13:22:03 +0000

    Simple changes in api route

--- api/generate.ts
+++ api/generate.ts
@@ -8,7 +8,7 @@ const configuration = new Configuration({
   apiKey
 });
 
-export default async function handler(
+export default function handler(
   req: VercelRequest,
   res: VercelResponse,
 ) {
@@ -16,7 +16,7 @@ export default async function handler(
   const { promptChat } = req.body.data
 
   const openai = new OpenAIApi(configuration);
-  const chatRes = await openai.createCompletion({
+  openai.createCompletion({
     model: "text-davinci-003",
     prompt: promptChat,
     temperature: 0.7,
@@ -24,7 +24,8 @@ export default async function handler(
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0
-  });
+  }).then(chatRes => {
+    res.status(200).json({summary: chatRes.data.choices[0].text})
+  })
 
-  res.json({summary: chatRes.data.choices[0].text})
 }
