ade97a7c8eb2fc8b30a4b585a978bef14ac45b02
SidiDev <sididev3@gmail.com>
2023-01-30 00:37:42 +0000

    Add test route in api

---
+++ api/test.ts
@@ -0,0 +1,9 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default async function handler(
+  req: VercelRequest,
+  res: VercelResponse,
+) {
+
+  res.json({hello: "Hello world"})
+}
